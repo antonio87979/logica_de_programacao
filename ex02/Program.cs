1f575db81575e425773fc01ae0d4289d2cbb454f
ANTONIO ROMEU MIGUEL DOS SANTOS NETO <antonio.rmsneto@senacsp.edu.br>
2025-01-13 19:56:39 -0300

    Aula 13/01

---
+++ ex02/Program.cs
@@ -0,0 +1,17 @@
+﻿//ex.02 -  escreva um programa que pergunte o nome do usuário, armazene
+//esse nome numa variável e apresente uma mensagem de boas-vindas exibindo o nome do usuário.
+
+//console = exibir no terminal/tela
+//writeline = exibir informação na linha ()
+//string = "caixa" de preenchimento. name = tipo de preechimento = 
+
+
+Console.WriteLine("informe seu nome");
+string name = Console.ReadLine();
+
+Console.WriteLine("informe sua idade");
+int numero = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("seja bem-vindo(a)"  + name);
+
---
+++ ex03/Program.cs
@@ -0,0 +1,18 @@
+﻿// escrever um programa que solicite dois números inteiros para o usuário e exiba a soma dos números
+
+//int.parse transfrma em numero inteiro
+//numerar as variaveis
+// int = variável usada para preencher caixas de texto com números interios
+
+Console.WriteLine("digite oprimeiro numero");
+int numero1 =int.Parse(Console.ReadLine());
+
+Console.WriteLine("digite o segundo número");
+int numero = int.Parse(Console.ReadLine());
+
+Console.WriteLine("digite o segundo número");
+int numero3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("a soma é:" + (numero1 + numero + numero3));
+
+//Console.WriteLine($"a soma é: {numero1 + numero + numero3}");  (com $ e {}, não é necessário o uso do sinal de adição)
